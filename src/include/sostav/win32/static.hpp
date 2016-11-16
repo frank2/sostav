d1928f7f772156ca0551702aa170c2e92c4a51bb
frank2 <frank2@dc949.org>
2016-11-16 12:50:59 -0800

    start adding subclassed win32 objects

---
+++ src/include/sostav/win32/static.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <windows.h>
+
+#include <string>
+
+#include "sostav/exception.hpp"
+#include "sostav/subclassedwindow.hpp"
+#include "sostav/types.hpp"
+
+namespace Sostav { namespace Win32
+{
+   typedef Exception StaticException;
+
+   class Static : public SubclassedWindow
+   {
+   public:
+      Static(
+
+   }}
